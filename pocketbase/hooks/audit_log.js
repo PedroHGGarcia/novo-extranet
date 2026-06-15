87680ac7b35d30a1603c83346b89c7bae2f040dd
Skip <skip@usecurling.com>
2026-06-15 12:54:56 -0300

    feat: Promote user to admin role

--- pocketbase/hooks/audit_log.js
+++ pocketbase/hooks/audit_log.js
@@ -7,14 +7,14 @@ onRecordAfterCreateSuccess((e) => {
     'regioes',
     'gerentes',
   ]
-  if (!collectionsToAudit.includes(e.collection.name)) return e.next()
+  if (!collectionsToAudit.includes(e.record.collection().name)) return e.next()
   if (!e.auth) return e.next()
 
   try {
     const audit = new Record($app.findCollectionByNameOrId('auditoria'))
     audit.set('user', e.auth.id)
     audit.set('acao', 'Create')
-    audit.set('tabela', e.collection.name)
+    audit.set('tabela', e.record.collection().name)
     audit.set('registro_id', e.record.id)
     audit.set('dados', JSON.parse(JSON.stringify(e.record)))
     $app.saveNoValidate(audit)
@@ -34,14 +34,14 @@ onRecordAfterUpdateSuccess((e) => {
     'regioes',
     'gerentes',
   ]
-  if (!collectionsToAudit.includes(e.collection.name)) return e.next()
+  if (!collectionsToAudit.includes(e.record.collection().name)) return e.next()
   if (!e.auth) return e.next()
 
   try {
     const audit = new Record($app.findCollectionByNameOrId('auditoria'))
     audit.set('user', e.auth.id)
     audit.set('acao', 'Update')
-    audit.set('tabela', e.collection.name)
+    audit.set('tabela', e.record.collection().name)
     audit.set('registro_id', e.record.id)
     audit.set('dados', JSON.parse(JSON.stringify(e.record)))
     $app.saveNoValidate(audit)
@@ -61,14 +61,14 @@ onRecordAfterDeleteSuccess((e) => {
     'regioes',
     'gerentes',
   ]
-  if (!collectionsToAudit.includes(e.collection.name)) return e.next()
+  if (!collectionsToAudit.includes(e.record.collection().name)) return e.next()
   if (!e.auth) return e.next()
 
   try {
     const audit = new Record($app.findCollectionByNameOrId('auditoria'))
     audit.set('user', e.auth.id)
     audit.set('acao', 'Delete')
-    audit.set('tabela', e.collection.name)
+    audit.set('tabela', e.record.collection().name)
     audit.set('registro_id', e.record.id)
     audit.set('dados', { deleted: true })
     $app.saveNoValidate(audit)
