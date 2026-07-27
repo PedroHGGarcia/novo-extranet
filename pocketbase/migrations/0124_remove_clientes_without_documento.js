b9e6f7e2969002e4bbe8f755221429fb4e8f50a8
Pedro Garcia <93056314+PedroHGGarcia@users.noreply.github.com>
2026-07-27 12:49:37 -0300

    feat: Remove clients without CNPJ

--- pocketbase/migrations/0124_remove_clientes_without_documento.js
+++ pocketbase/migrations/0124_remove_clientes_without_documento.js
@@ -24,7 +24,7 @@ migrate(
     var totalDeleted = 0
     var totalDocsDeleted = 0
     var totalPropostasUpdated = 0
-    var totalProjetosUpdated = 0
+    var totalProjetosDeleted = 0
 
     app.runInTransaction(function (txApp) {
       for (var i = 0; i < clientesSemDocumento.length; i++) {
@@ -43,6 +43,34 @@ migrate(
           totalDocsDeleted++
         }
 
+        var projetos = txApp.findRecordsByFilter(
+          'projetos',
+          "cliente = '" + clienteId + "'",
+          '-created',
+          10000,
+          0,
+        )
+
+        var projetoIds = []
+        for (var pr = 0; pr < projetos.length; pr++) {
+          projetoIds.push(projetos[pr].id)
+        }
+
+        for (var pi = 0; pi < projetoIds.length; pi++) {
+          var propostasDoProjeto = txApp.findRecordsByFilter(
+            'propostas',
+            "projeto = '" + projetoIds[pi] + "'",
+            '-created',
+            10000,
+            0,
+          )
+          for (var pp = 0; pp < propostasDoProjeto.length; pp++) {
+            propostasDoProjeto[pp].set('projeto', null)
+            txApp.save(propostasDoProjeto[pp])
+            totalPropostasUpdated++
+          }
+        }
+
         var propostas = txApp.findRecordsByFilter(
           'propostas',
           "cliente = '" + clienteId + "'",
@@ -56,35 +84,38 @@ migrate(
           totalPropostasUpdated++
         }
 
-        var projetos = txApp.findRecordsByFilter(
-          'projetos',
-          "cliente = '" + clienteId + "'",
-          '-created',
-          10000,
-          0,
-        )
-        for (var pr = 0; pr < projetos.length; pr++) {
-          projetos[pr].set('cliente', null)
-          txApp.save(projetos[pr])
-          totalProjetosUpdated++
+        for (var pd = 0; pd < projetos.length; pd++) {
+          if (adminUserId) {
+            var projAuditRec = new Record(auditoriaCol)
+            projAuditRec.set('user', adminUserId)
+            projAuditRec.set('acao', 'delete')
+            projAuditRec.set('tabela', 'projetos')
+            projAuditRec.set('registro_id', projetos[pd].id)
+            projAuditRec.set('dados', {
+              nome: projetos[pd].getString('nome') || '',
+              cliente_id: clienteId,
+              id: projetos[pd].id,
+            })
+            txApp.save(projAuditRec)
+          }
+          txApp.delete(projetos[pd])
+          totalProjetosDeleted++
         }
 
-        var auditRec = new Record(auditoriaCol)
         if (adminUserId) {
+          var auditRec = new Record(auditoriaCol)
           auditRec.set('user', adminUserId)
-        } else {
-          continue
+          auditRec.set('acao', 'delete')
+          auditRec.set('tabela', 'clientes')
+          auditRec.set('registro_id', clienteId)
+          auditRec.set('dados', {
+            fantasia: cliente.getString('fantasia') || '',
+            documento: cliente.getString('documento') || '',
+            razao_social: cliente.getString('razao_social') || '',
+            id: clienteId,
+          })
+          txApp.save(auditRec)
         }
-        auditRec.set('acao', 'delete')
-        auditRec.set('tabela', 'clientes')
-        auditRec.set('registro_id', clienteId)
-        auditRec.set('dados', {
-          fantasia: cliente.getString('fantasia') || '',
-          documento: cliente.getString('documento') || '',
-          razao_social: cliente.getString('razao_social') || '',
-          id: clienteId,
-        })
-        txApp.save(auditRec)
 
         txApp.delete(cliente)
         totalDeleted++
@@ -99,8 +130,8 @@ migrate(
         ' documentos_clientes excluídos, ' +
         totalPropostasUpdated +
         ' propostas atualizadas, ' +
-        totalProjetosUpdated +
-        ' projetos atualizados.',
+        totalProjetosDeleted +
+        ' projetos excluídos.',
     )
   },
   (app) => {
